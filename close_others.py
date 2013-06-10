d04e62477219e5c5eb353a8e34ffa8e69deac2b0
suisho <suisho000000@gmail.com>
2013-06-10 23:23:01 +0900

    initial commit

---
+++ close_others.py
@@ -0,0 +1,13 @@
+#import sublime
+import sublime_plugin
+
+
+class CloseOthers(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        window = self.view.window()
+        group_index, view_index = window.get_view_index(self.view)
+        window.run_command("close_others_by_index",
+                          {"group": group_index,
+                           "index": view_index}
+                          )
